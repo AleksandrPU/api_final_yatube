7b56091fb57a4da71662ba361ab06691f95d36a1
Паутов Александр <sh2d000w@gmail.com>
2023-11-04 16:54:20 +0300

    fix: change default ordering for model Post in posts/models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -35,7 +35,7 @@ class Post(AuthorTextModel):
         default_related_name = 'posts'
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-        ordering = ('-pub_date',)
+        ordering = ('pub_date',)
 
     def __str__(self):
         return self.text[:STRING_LENGTH_LIMIT]
